a95453386d4c9960bef98f01d94d64f18c7beeb9
arvin <1095654362@qq.com>
2019-05-04 02:37:33 +0800

    博客项目

---
+++ think_blog/app/index/controller/Index.php
@@ -0,0 +1,15 @@
+<?php
+namespace app\index\controller;
+
+class Index
+{
+    public function index()
+    {
+        return view();
+        //dump('_CSS_');
+    }
+    
+    public function login(){  	
+    	return view();
+    }
+}
---
+++ think_blog/conf/config.php
@@ -0,0 +1,12 @@
+<?php
+
+   return[
+   
+           //视图输出字符串内容替换
+             'view_replace_str' => [
+                
+                  '_IMG_' => '/think_blog/public/static/img'
+             ],
+   
+   ]
+?>
